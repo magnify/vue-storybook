64116f1027cdd5a2c9b2ee0f97c5c128677aeb6e
Brian Stefan Jensen <bsj@novicell.dk>
2019-09-23 14:44:39 +0200

    Fixed projects

--- some-project/README.md
+++ some-project/README.md
@@ -1,4 +1,4 @@
-# project1
+# some-project
 
 ## Project setup
 ```
--- storybook/.storybook/config.js
+++ storybook/.storybook/config.js
@@ -1,9 +1,9 @@
 import { configure } from "@storybook/vue";
 import Vue from "vue";
-import List from "../components/list/List.vue";
+import List from "../src/components/list/List.vue";
 Vue.component("List", List);
 function loadStories() {
-  const req = require.context("../components", true, /\.stories\.js$/);
+  const req = require.context("../src/components", true, /\.stories\.js$/);
   req.keys().forEach(filename => req(filename));
 }
 configure(loadStories, module);
